7bfe96824126eaddc63ee1e90915892fb5419e60
Levin Huang <lpotato4@gmail.com>
2016-11-27 14:59:49 -0500

    implemented brute force all winners method and getting winners from edge list input. also created branched graph class

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from prefpy.profile import Profile
 
 if __name__ == '__main__' :
 
-    data=Profile({},[])
+	data=Profile({},[])
 
-    data.importPreflibFile("input")
-
-    rankpairMech=RankedPairs()
-    print(rankpairMech.getOneWinner(data))
+	data.importPreflibFile("input")
+	
+	rankpairMech=RankedPairs()
+	myList = [(4, 'a', 'b'), (3, 'b', 'c'), (3, 'c', 'd'), (2, 'd', 'b'), (2, 'c', 'a'), (4, 'd', 'a')]
+	print(rankpairMech.getWinners(edges = myList))
+	#print(rankpairMech.getWinners(prof = data))
+    #print(rankpairMech.getOneWinner(data))
     #edgeList=rankpairMech.getSortedEdges(data)
     #rankpairMech.createNXGraph(edgeList)
--- prefpy/rankedPairs.py
+++ prefpy/rankedPairs.py
@@ -6,6 +6,46 @@ from prefpy.preference import Preference
 from prefpy.mechanism import Mechanism
 import networkx as nx
 import matplotlib.pyplot as plt
+import copy
+
+class branchedGraph():
+	def __init__(self, rL, DG):
+		self.remainingList = rL
+		self.DG = DG
+		
+	def getNextEdge(self):
+		branchedGraphList = []
+		branchedEdges, branchedRemEdges = self.findTies()
+
+		for i in range(len(branchedEdges)):
+			tmpGraph = self.DG.copy()
+			tmpEdge = branchedEdges[i]
+			tmpGraph.add_edge(tmpEdge[1], tmpEdge[2], weight=tmpEdge[0])
+			try: 
+				nx.find_cycle(tmpGraph)
+			except:
+				tmpBranchedGraph = branchedGraph(branchedRemEdges[i], tmpGraph)
+				branchedGraphList.append(tmpBranchedGraph)
+		return branchedGraphList
+			
+		
+	def findTies(self):
+		branchedRemEdges = [] #list of lists
+		edges = self.remainingList
+		tmpList = []
+		tmpRemEdges = []
+		if len(edges) > 0:
+			tmpWeight = edges[0][0]
+			
+			for i in range(len(edges)):
+				if edges[i][0] == tmpWeight:
+					tmpRemEdges = copy.copy(edges)
+					del tmpRemEdges[i]
+					branchedRemEdges.append(tmpRemEdges)
+					tmpList.append(edges[i])
+		
+		return tmpList, branchedRemEdges
+	
 
 class RankedPairs(Mechanism):
 	def __init__(self):
@@ -22,7 +62,7 @@ class RankedPairs(Mechanism):
 		labels = nx.get_edge_attributes(DG, 'weight')
 		nx.draw_networkx_edge_labels(DG, pos=nodeLayout, edge_labels=labels)
 		plt.show()
-
+		
 	#returns edges from the wmg sorted by weights
 	def getSortedEdges(self,prof):
 		# newGraph={}
@@ -37,13 +77,22 @@ class RankedPairs(Mechanism):
 				if cand1 is not cand2:
 					if wmg[cand1][cand2] >= 0:
 						edges.append((wmg[cand1][cand2], prof.candMap[cand1], prof.candMap[cand2]))
-		sorted(edges, key=lambda weight: weight[0])
+		edges = sorted(edges, key=lambda weight: weight[0], reverse = True)
 
 		return edges
+		
+	def drawGraph(self, DG):
+		#nx.draw(DG)
+		plt.figure()
+		nodeLayout=nx.spring_layout(DG)
+		nx.draw_networkx(DG,pos=nodeLayout,arrows=True)
+		labels = nx.get_edge_attributes(DG, 'weight')
+		nx.draw_networkx_edge_labels(DG, pos=nodeLayout, edge_labels=labels)
+		#plt.show()
+		
+		
 	def getNewGraph(self,prof):
-
 		wmg=prof.getWmg()
-
 		DG=nx.DiGraph()
 		newGraph=nx.DiGraph()
 		edges=[]
@@ -55,7 +104,6 @@ class RankedPairs(Mechanism):
 			DG.add_edge(edge[1], edge[2], weight=edge[0])
 			try:
 				#if there is no cycle the method throws an exception
-
 				nx.find_cycle(DG)
 				DG.remove_edge(edge[1], edge[2])
 				break
@@ -73,14 +121,50 @@ class RankedPairs(Mechanism):
 			inedges = newGraph.in_edges(i)
 			if inedges == []:
 				winners.append(i)
-
 		return winners
 
 	def getWinnerWithTieBreakingMech(self,prof,prefList):
 		pass
+		
+	def getWinner(self, graphList):
+		winners = []
+		for bGraph in graphList:
+			newGraph = bGraph.DG
+			for i in newGraph.nodes():
+				inedges = newGraph.in_edges(i)
+				if inedges == []:
+					winners.append(i)
+		return winners
+		
 
-	def getWinners(self, prof):
-		pass
+	def getWinners(self, prof=None,edges=None):
+		if edges is None:
+			edges = self.getSortedEdges(prof)
+		else:
+			edges = sorted(edges, key=lambda weight: weight[0], reverse = True)
+		print(edges)
+		DG = nx.DiGraph()
+		newBranchedGraph = branchedGraph(edges, DG)
+		graphs = newBranchedGraph.getNextEdge()
+		winners = []
+		doneList = []
+		while(len(graphs) != 0):
+			tmpGraphs = []
+			
+			for graph in graphs:
+				tmpNextEdgeList = graph.getNextEdge()
+				if tmpNextEdgeList == []:
+					doneList.append(graph)
+				else:
+					tmpGraphs = tmpGraphs + tmpNextEdgeList
+			graphs = copy.copy(tmpGraphs)
+				
+		winners = self.getWinner(doneList)
+		for winner in doneList:
+			self.drawGraph(winner.DG)
+		plt.show()
+		
+		return set(winners)
 
 
 	def getRanking(self, prof):
